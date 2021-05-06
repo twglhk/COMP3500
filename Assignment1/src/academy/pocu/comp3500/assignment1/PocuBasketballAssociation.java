6cd9e8db4ac88e643d213fe643d247fdf1ed20fb
John <twglhk@gmail.com>
2021-05-06 09:57:45 +0900

    update : ass1 - 2-2

--- Assignment1/src/academy/pocu/comp3500/assignment1/PocuBasketballAssociation.java
+++ Assignment1/src/academy/pocu/comp3500/assignment1/PocuBasketballAssociation.java
@@ -67,7 +67,42 @@ public final class PocuBasketballAssociation {
     }
 
     public static Player findPlayerPointsPerGame(final Player[] players, int targetPoints) {
-        return null;
+        // pointsPerGame에 가장 가까운 선수들을 찾음 (별도의 컨테이너 저장은 불가능하니 순차적으로 탐색하면서 조건 검사)
+        // 배열을 순회하면서 득점에 가장 가까운 사람을 저장. 그 사람의 득점 차이도 저장
+
+        // 초기값 업데이트
+        Player resultPlayer = players[0];
+        var minPointsGap = Math.abs(targetPoints - resultPlayer.getPointsPerGame());
+
+        for (int i = 1; i < players.length; ++i)
+        {
+            var pointsGap = Math.abs(targetPoints - players[i].getPointsPerGame());
+
+            // 이전 플레이어의 타깃 득점 차이보다 높다면 패스
+            if (pointsGap > minPointsGap)
+                continue;
+
+            // 이전에 득점 차이 보다 새로운 선수의 득점 차이가 적다면 그 사람으로 대체
+            else if (pointsGap < minPointsGap)
+            {
+                resultPlayer = players[i];
+                minPointsGap = pointsGap;
+                continue;
+            }
+
+            // 득점 차이가 동일하다면
+            else
+            {
+                //이전에 저장된 선수보다 평균 득점이 높을 경우에만 업데이트, 아니면 패스
+                if (resultPlayer.getPointsPerGame() < players[i].getPointsPerGame())
+                {
+                    resultPlayer = players[i];
+                    minPointsGap = pointsGap;
+                }
+            }
+        }
+
+        return resultPlayer;
     }
 
     public static Player findPlayerShootingPercentage(final Player[] players, int targetShootingPercentage) {
--- Assignment1/src/academy/pocu/comp3500/assignment1/app/Program.java
+++ Assignment1/src/academy/pocu/comp3500/assignment1/app/Program.java
@@ -6,27 +6,50 @@ import academy.pocu.comp3500.assignment1.pba.GameStat;
 public class Program {
 
     public static void main(String[] args) {
-        GameStat[] gameStats = new GameStat[] {
-                new GameStat("Player 1", 1, 13, 5, 6, 10, 1),
-                new GameStat("Player 2", 2, 5, 2, 5, 0, 10),
-                new GameStat("Player 1", 3, 12, 6, 9, 8, 5),
-                new GameStat("Player 3", 1, 31, 15, 40, 5, 3),
-                new GameStat("Player 2", 1, 3, 1, 3, 12, 2),
-                new GameStat("Player 1", 2, 11, 6, 11, 9, 3),
-                new GameStat("Player 2", 3, 9, 3, 3, 1, 11),
-                new GameStat("Player 3", 4, 32, 15, 51, 4, 2),
-                new GameStat("Player 4", 3, 44, 24, 50, 1, 1),
-                new GameStat("Player 1", 4, 11, 5, 14, 8, 3),
-                new GameStat("Player 2", 4, 5, 1, 3, 1, 9),
-        };
+        {
+            GameStat[] gameStats = new GameStat[] {
+                    new GameStat("Player 1", 1, 13, 5, 6, 10, 1),
+                    new GameStat("Player 2", 2, 5, 2, 5, 0, 10),
+                    new GameStat("Player 1", 3, 12, 6, 9, 8, 5),
+                    new GameStat("Player 3", 1, 31, 15, 40, 5, 3),
+                    new GameStat("Player 2", 1, 3, 1, 3, 12, 2),
+                    new GameStat("Player 1", 2, 11, 6, 11, 9, 3),
+                    new GameStat("Player 2", 3, 9, 3, 3, 1, 11),
+                    new GameStat("Player 3", 4, 32, 15, 51, 4, 2),
+                    new GameStat("Player 4", 3, 44, 24, 50, 1, 1),
+                    new GameStat("Player 1", 4, 11, 5, 14, 8, 3),
+                    new GameStat("Player 2", 4, 5, 1, 3, 1, 9),
+            };
 
-        Player[] players = new Player[] {
-                new Player(),
-                new Player(),
-                new Player(),
-                new Player()
-        };
+            Player[] players = new Player[] {
+                    new Player(),
+                    new Player(),
+                    new Player(),
+                    new Player()
+            };
 
-        PocuBasketballAssociation.processGameStats(gameStats, players);
+            PocuBasketballAssociation.processGameStats(gameStats, players);
+        }
+
+        {
+            Player[] players = new Player[] {
+                    new Player("Player 1", 1, 5, 1, 60),
+                    new Player("Player 2", 5, 2, 11, 31),
+                    new Player("Player 3", 7, 4, 7, 44),
+                    new Player("Player 4", 10, 10, 15, 25),
+                    new Player("Player 5", 11, 12, 6, 77),
+                    new Player("Player 6", 15, 0, 12, 61),
+                    new Player("Player 7", 16, 8, 2, 70)
+            };
+
+            Player player = PocuBasketballAssociation.findPlayerPointsPerGame(players, 12); // player: Player 5
+            System.out.println(player.getName());
+
+            player = PocuBasketballAssociation.findPlayerPointsPerGame(players, 5); // player: Player 2
+            System.out.println(player.getName());
+
+            player = PocuBasketballAssociation.findPlayerPointsPerGame(players, 13); // player: Player 6
+            System.out.println(player.getName());
+        }
     }
 }
