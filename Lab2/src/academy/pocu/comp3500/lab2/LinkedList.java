343f9a5a1cfd5e6d6a02c1ce36f7ddb2558a4b6c
John <twglhk@gmail.com>
2021-05-13 21:41:36 +0900

    update : Linked-List(getIndexOf, getOrNull)

--- Lab2/src/academy/pocu/comp3500/lab2/LinkedList.java
+++ Lab2/src/academy/pocu/comp3500/lab2/LinkedList.java
@@ -81,10 +81,27 @@ public final class LinkedList {
     }
 
     public static int getIndexOf(final Node rootOrNull, final int data) {
+
+        var currentNode = rootOrNull;
+        var currentIndex = 0;
+        while (currentNode != null) {
+            if (currentNode.getData() == data)
+                return currentIndex;
+            currentNode = currentNode.getNextOrNull();
+            currentIndex++;
+        }
         return -1;
     }
 
     public static Node getOrNull(final Node rootOrNull, final int index) {
+        var currentNode = rootOrNull;
+        var currentIndex = 0;
+        while (currentNode != null) {
+            if (currentIndex == index)
+                return currentNode;
+            currentNode = currentNode.getNextOrNull();
+            currentIndex++;
+        }
         return null;
     }
 
