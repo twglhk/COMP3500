7fb2691543e1b0cac18593d898d60d76ca42ceb4
John <twglhk@gmail.com>
2021-05-20 21:03:59 +0900

    add : missioncontrol init

---
+++ Lab3/src/academy/pocu/comp3500/lab3/MissionControl.java
@@ -0,0 +1,16 @@
+package academy.pocu.comp3500.lab3;
+
+import java.util.ArrayList;
+
+public final class MissionControl {
+    private MissionControl() {
+    }
+
+    public static int findMaxAltitudeTime(final int[] altitudes) {
+        return -1;
+    }
+
+    public static ArrayList<Integer> findAltitudeTimes(final int[] altitudes, final int targetAltitude) {
+        return null;
+    }
+}
