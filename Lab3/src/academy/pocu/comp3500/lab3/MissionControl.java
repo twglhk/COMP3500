b950e98c3b8c5d423d70c4bfcc355781297fa2c7
John <twglhk@gmail.com>
2021-05-21 14:47:15 +0900

    update : lab3 findMaxAltitudeTime return value fix

--- Lab3/src/academy/pocu/comp3500/lab3/MissionControl.java
+++ Lab3/src/academy/pocu/comp3500/lab3/MissionControl.java
@@ -8,24 +8,25 @@ public final class MissionControl {
 
     public static int findMaxAltitudeTime(final int[] altitudes) {
 
-        return findMaxAltitudeRecursiveBinarySearch(altitudes, 0, altitudes.length-1);
+        return findMaxAltitudeRecursiveBinarySearch(altitudes, 0, altitudes.length - 1);
     }
 
     public static int findMaxAltitudeRecursiveBinarySearch(final int[] altitudes, int left, int right) {
         if (left == right)
-            return altitudes[left];
+            return left;
 
         int mid = (left + right) / 2;
-        if (altitudes[mid] >= altitudes[mid-1] && altitudes[mid] >= altitudes[mid+1])
-            return altitudes[mid];
+        if (altitudes[mid] >= altitudes[mid - 1] && altitudes[mid] >= altitudes[mid + 1])
+            return mid;
 
-        if (altitudes[mid] < altitudes[mid-1])
+        if (altitudes[mid] < altitudes[mid - 1])
             return findMaxAltitudeRecursiveBinarySearch(altitudes, left, mid);
         else
             return findMaxAltitudeRecursiveBinarySearch(altitudes, mid + 1, right);
     }
 
     public static ArrayList<Integer> findAltitudeTimes(final int[] altitudes, final int targetAltitude) {
+
         return null;
     }
 }
