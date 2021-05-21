6b5baa4ae2255283a1b9844d9b369e44ee833ecc
John <twglhk@gmail.com>
2021-05-21 14:38:09 +0900

    update : lab3 - findMaxAltitueTime

--- Lab3/src/academy/pocu/comp3500/lab3/MissionControl.java
+++ Lab3/src/academy/pocu/comp3500/lab3/MissionControl.java
@@ -7,7 +7,22 @@ public final class MissionControl {
     }
 
     public static int findMaxAltitudeTime(final int[] altitudes) {
-        return -1;
+
+        return findMaxAltitudeRecursiveBinarySearch(altitudes, 0, altitudes.length-1);
+    }
+
+    public static int findMaxAltitudeRecursiveBinarySearch(final int[] altitudes, int left, int right) {
+        if (left == right)
+            return altitudes[left];
+
+        int mid = (left + right) / 2;
+        if (altitudes[mid] >= altitudes[mid-1] && altitudes[mid] >= altitudes[mid+1])
+            return altitudes[mid];
+
+        if (altitudes[mid] < altitudes[mid-1])
+            return findMaxAltitudeRecursiveBinarySearch(altitudes, left, mid);
+        else
+            return findMaxAltitudeRecursiveBinarySearch(altitudes, mid + 1, right);
     }
 
     public static ArrayList<Integer> findAltitudeTimes(final int[] altitudes, final int targetAltitude) {
--- Lab3/src/academy/pocu/comp3500/lab3/app/Program.java
+++ Lab3/src/academy/pocu/comp3500/lab3/app/Program.java
@@ -1,8 +1,18 @@
 package academy.pocu.comp3500.lab3.app;
 
+import academy.pocu.comp3500.lab3.MissionControl;
+
 public class Program {
 
     public static void main(String[] args) {
 	    // write your code here
+        {
+            final int[] altitudes = new int[] { 1, 2, 3, 4, 5, 6, 7, 4, 3, 2 };
+
+            final int maxAltitudeTime = MissionControl.findMaxAltitudeTime(altitudes);
+
+            System.out.println(maxAltitudeTime);
+            assert (maxAltitudeTime == 6);
+        }
     }
 }
