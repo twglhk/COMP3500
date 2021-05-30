35af50fe98db6c5f44f92ed25b0e7e9a52bc41eb
John <twglhk@gmail.com>
2021-05-30 19:46:00 +0900

    add : ass2 - init

---
+++ Assignment2/src/academy/pocu/comp3500/assignment2/Indent.java
@@ -0,0 +1,7 @@
+package academy.pocu.comp3500.assignment2;
+
+public final class Indent {
+    public void discard() {
+
+    }
+}
---
+++ Assignment2/src/academy/pocu/comp3500/assignment2/Logger.java
@@ -0,0 +1,29 @@
+package academy.pocu.comp3500.assignment2;
+
+import java.io.BufferedWriter;
+
+public final class Logger {
+    public static void log(final String text) {
+        
+    }
+
+    public static void printTo(final BufferedWriter writer) {
+
+    }
+
+    public static void printTo(final BufferedWriter writer, final String filter) {
+
+    }
+
+    public static void clear() {
+
+    }
+
+    public static Indent indent() {
+        return null;
+    }
+
+    public static void unindent() {
+
+    }
+}
