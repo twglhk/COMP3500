6c954794de640401e1e51f6b86d2b067b773a7c4
John <twglhk@gmail.com>
2021-06-04 20:15:35 +0900

    lab5 init

---
+++ Lab5/src/academy/pocu/comp3500/lab5/Bank.java
@@ -0,0 +1,15 @@
+package academy.pocu.comp3500.lab5;
+
+public class Bank {
+    public Bank(byte[][] pubKeys, final long[] amounts) {
+
+    }
+
+    public long getBalance(final byte[] pubKey) {
+        return -1;
+    }
+
+    public boolean transfer(final byte[] from, byte[] to, final long amount, final byte[] signature) {
+        return false;
+    }
+}
---
+++ Lab5/src/academy/pocu/comp3500/lab5/KeyGenerator.java
@@ -0,0 +1,9 @@
+package academy.pocu.comp3500.lab5;
+
+import java.math.BigInteger;
+
+public class KeyGenerator {
+    public static boolean isPrime(final BigInteger number) {
+        return false;
+    }
+}
