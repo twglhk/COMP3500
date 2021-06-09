453ca318ce2962bdbb8b46bafaa72400d9908a1a
John <twglhk@gmail.com>
2021-06-09 18:48:35 +0900

    update : update logger

--- Assignment2/src/academy/pocu/comp3500/assignment2/Logger.java
+++ Assignment2/src/academy/pocu/comp3500/assignment2/Logger.java
@@ -1,14 +1,25 @@
 package academy.pocu.comp3500.assignment2;
 
+import academy.pocu.comp3500.assignment2.datastructure.LinkedList;
+import academy.pocu.comp3500.assignment2.datastructure.Queue;
+
+import javax.management.QueryEval;
 import java.io.BufferedWriter;
+import java.io.IOException;
 
 public final class Logger {
-    public static void log(final String text) {
+    private static LinkedList<String> logList  = new LinkedList<String>();
 
+    public static void log(final String text) {
+        logList.add(text);
     }
 
-    public static void printTo(final BufferedWriter writer) {
-
+    public static void printTo(final BufferedWriter writer) throws IOException {
+        for (var log : logList) {
+            writer.write(log);
+            writer.newLine();
+        }
+        writer.flush();
     }
 
     public static void printTo(final BufferedWriter writer, final String filter) {
@@ -16,7 +27,7 @@ public final class Logger {
     }
 
     public static void clear() {
-
+        logList.clear();
     }
 
     public static Indent indent() {
--- Assignment2/src/academy/pocu/comp3500/assignment2/app/Program.java
+++ Assignment2/src/academy/pocu/comp3500/assignment2/app/Program.java
@@ -1,8 +1,28 @@
 package academy.pocu.comp3500.assignment2.app;
 
+import academy.pocu.comp3500.assignment2.Indent;
+import academy.pocu.comp3500.assignment2.Logger;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static academy.pocu.comp3500.assignment2.Logger.log;
+
 public class Program {
 
     public static void main(String[] args) {
-	    // write your code here
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("mylog1.log"));
+            log("hello");
+            log("world");
+            log("this is logging at the top level");
+
+            Logger.printTo(writer);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
