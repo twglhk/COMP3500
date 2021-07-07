b1de136120b99ce34b6633df473bf7be1ee70afc
John <twglhk@gmail.com>
2021-07-07 17:10:49 +0900

    update : lab8, init

---
+++ Lab8/src/academy/pocu/comp3500/lab8/MazeSolver.java
@@ -0,0 +1,11 @@
+package academy.pocu.comp3500.lab8;
+
+import academy.pocu.comp3500.lab8.maze.Point;
+
+import java.util.List;
+
+public final class MazeSolver {
+    public static List<Point> findPath(final char[][] maze, final Point start) {
+        return null;
+    }
+}
