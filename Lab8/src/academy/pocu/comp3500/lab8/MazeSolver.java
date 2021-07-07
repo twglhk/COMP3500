ad224b3d5619dfb2b1e66c634ad2bd69830c2f20
John <twglhk@gmail.com>
2021-07-07 19:30:04 +0900

    update : lab8 update

--- Lab8/src/academy/pocu/comp3500/lab8/MazeSolver.java
+++ Lab8/src/academy/pocu/comp3500/lab8/MazeSolver.java
@@ -2,10 +2,63 @@ package academy.pocu.comp3500.lab8;
 
 import academy.pocu.comp3500.lab8.maze.Point;
 
+import java.security.cert.PolicyNode;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public final class MazeSolver {
+    private static char wall = 'x';
+    private static char exit = 'E';
+    private static char path = ' ';
+
+
     public static List<Point> findPath(final char[][] maze, final Point start) {
-        return null;
+        Stack<Point> pathStack = new Stack<Point>();
+        LinkedList<Point> resultList = new LinkedList<>();
+        boolean[][] visit = new boolean[maze.length][maze[0].length];
+        FindPathRecursive(maze, visit, start, pathStack, resultList);
+
+        for(var point : resultList) {
+            System.out.print("[" + point.getX() + " , " + point.getY() + "]");
+        }
+
+        System.out.println();
+        return resultList;
+    }
+
+    public static void FindPathRecursive(final char[][] maze, final boolean[][] visit, final Point currentPoint, final Stack<Point> pathStack, final LinkedList<Point> resultList) {
+        var xPoint = currentPoint.getX();
+        var yPoint = currentPoint.getY();
+        if (visit[yPoint][xPoint])
+            return;
+
+        visit[yPoint][xPoint] = true;
+        pathStack.push(currentPoint);
+
+        var mazePointInfo = maze[yPoint][xPoint];
+        if (mazePointInfo == exit) {
+            resultList.addAll(pathStack);
+        } else if (mazePointInfo == wall) {
+            pathStack.pop();
+        } else if (mazePointInfo == path) {
+            // Left
+            if (xPoint != 0) {
+                FindPathRecursive(maze, visit, new Point(xPoint - 1, yPoint), pathStack, resultList);
+            }
+            // Right
+            if (xPoint != maze[0].length - 1) {
+                FindPathRecursive(maze, visit, new Point(xPoint + 1, yPoint), pathStack, resultList);
+            }
+            // Up
+            if (yPoint != 0) {
+                FindPathRecursive(maze, visit, new Point(xPoint, yPoint - 1), pathStack, resultList);
+            }
+            // Down
+            if (yPoint != maze.length - 1) {
+                FindPathRecursive(maze, visit, new Point(xPoint, yPoint + 1), pathStack, resultList);
+            }
+            pathStack.pop();
+        }
     }
 }
--- Lab8/src/academy/pocu/comp3500/lab8/app/Program.java
+++ Lab8/src/academy/pocu/comp3500/lab8/app/Program.java
@@ -1,8 +1,76 @@
 package academy.pocu.comp3500.lab8.app;
 
+import academy.pocu.comp3500.lab8.MazeSolver;
+import academy.pocu.comp3500.lab8.maze.Point;
+
+import java.util.List;
+
 public class Program {
 
     public static void main(String[] args) {
-	    // write your code here
+
+        char[][] maze3x3 = new char[][]{
+                {'x', 'x', 'x'},
+                {'x', 'E', 'x'},
+                {'x', 'x', 'x'}
+        };
+
+        List<Point> result = MazeSolver.findPath(maze3x3, new Point(1, 1));
+
+        assert (result.size() == 1);
+        assert (result.get(0).getX() == 1 && result.get(0).getY() == 1);
+
+        char[][] maze3x5 = new char[][]{
+                {'x', 'x', 'x'},
+                {'x', ' ', 'x'},
+                {'x', ' ', 'x'},
+                {'x', 'E', 'x'},
+                {'x', 'x', 'x'}
+        };
+
+        result = MazeSolver.findPath(maze3x5, new Point(1, 1));
+
+        assert (result.size() == 3);
+        assert (result.get(0).getX() == 1 && result.get(0).getY() == 1);
+        assert (result.get(1).getX() == 1 && result.get(1).getY() == 2);
+        assert (result.get(2).getX() == 1 && result.get(2).getY() == 3);
+
+        char[][] maze7x5 = new char[][]{
+                {'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+                {'x', 'E', 'x', ' ', ' ', ' ', 'x'},
+                {'x', ' ', 'x', ' ', 'x', ' ', 'x'},
+                {'x', ' ', ' ', ' ', 'x', ' ', 'x'},
+                {'x', 'x', 'x', 'x', 'x', 'x', 'x'}
+        };
+
+        result = MazeSolver.findPath(maze7x5, new Point(5, 3));
+
+        assert (result.size() == 11);
+        assert (result.get(0).getX() == 5 && result.get(0).getY() == 3);
+        assert (result.get(1).getX() == 5 && result.get(1).getY() == 2);
+        assert (result.get(2).getX() == 5 && result.get(2).getY() == 1);
+        assert (result.get(3).getX() == 4 && result.get(3).getY() == 1);
+        assert (result.get(4).getX() == 3 && result.get(4).getY() == 1);
+        assert (result.get(5).getX() == 3 && result.get(5).getY() == 2);
+        assert (result.get(6).getX() == 3 && result.get(6).getY() == 3);
+        assert (result.get(7).getX() == 2 && result.get(7).getY() == 3);
+        assert (result.get(8).getX() == 1 && result.get(8).getY() == 3);
+        assert (result.get(9).getX() == 1 && result.get(9).getY() == 2);
+        assert (result.get(10).getX() == 1 && result.get(10).getY() == 1);
+
+        char[][] maze8x6 = new char[][]{
+                {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'},
+                {'x', 'x', ' ', 'x', ' ', 'E', ' ', 'x'},
+                {'x', 'x', ' ', 'x', ' ', 'x', 'x', 'x'},
+                {'x', ' ', ' ', ' ', ' ', 'x', ' ', 'x'},
+                {'x', 'x', ' ', 'x', ' ', ' ', ' ', 'x'},
+                {'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x'}
+        };
+
+        result = MazeSolver.findPath(maze8x6, new Point(2, 2));
+
+        assert (result.size() == 7);
+        assert (result.get(0).getX() == 2 && result.get(0).getY() == 2);
+        assert (result.get(6).getX() == 5 && result.get(6).getY() == 1);
     }
 }
