4212a5ddf7ce801beb83bb31afa69f56c3ecc0c6
John <twglhk@gmail.com>
2021-07-10 12:41:08 +0900

    update : ass3 - Player constructor

--- Assignment3/src/academy/pocu/comp3500/assignment3/Player.java
+++ Assignment3/src/academy/pocu/comp3500/assignment3/Player.java
@@ -1,5 +1,20 @@
 package academy.pocu.comp3500.assignment3;
+import academy.pocu.comp3500.assignment3.chess.Move;
+import academy.pocu.comp3500.assignment3.chess.PlayerBase;
 
-public class Player {
+public class Player extends PlayerBase {
+    public Player(boolean isWhite, int maxMoveTimeMilliseconds) {
+        super(isWhite, maxMoveTimeMilliseconds);
 
+    }
+
+    @Override
+    public Move getNextMove(char[][] board) {
+        return null;
+    }
+
+    @Override
+    public Move getNextMove(char[][] board, Move opponentMove) {
+        return null;
+    }
 }
