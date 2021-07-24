1c8e2adaa31c75a155b3f061182ab306deb7a4df
John <twglhk@gmail.com>
2021-07-24 19:25:56 +0900

    update : lab10 init

---
+++ Lab10/src/academy/pocu/comp3500/lab10/Project.java
@@ -0,0 +1,11 @@
+package academy.pocu.comp3500.lab10;
+
+import academy.pocu.comp3500.lab10.project.Task;
+
+import java.util.List;
+
+public class Project {
+    public static List<String> findSchedule(final Task[] tasks, final boolean includeMaintenance) {
+        return null;
+    }
+}
