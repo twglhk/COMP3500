36af7c25c4d2c508e3a78df636a605e64ac46a93
John <twglhk@gmail.com>
2021-07-27 23:12:20 +0900

    update : lab11 BallBoy

--- Lab11/src/academy/pocu/comp3500/lab11/BallBoy.java
+++ Lab11/src/academy/pocu/comp3500/lab11/BallBoy.java
@@ -2,6 +2,8 @@ package academy.pocu.comp3500.lab11;
 
 import academy.pocu.comp3500.lab11.data.Point;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 //LinkedList
 //ArrayList
@@ -18,6 +20,38 @@ public class BallBoy {
         //볼보이는 언제나 x = 0, y = 0 위치에서 시작하며 일이 끝나면 이 위치로 다시 돌아와야 합니다. 즉, 반환된 경로의 시작과 끝은 언제나 이 위치여야 합니다.
         //시작 위치에 골프공이 있는 경우는 없습니다.
 
-        return null;
+        ArrayList<Point> result = new ArrayList<Point>();
+        Point startPos = new Point(0, 0);
+        Point currentPos = startPos;
+        result.add(startPos);
+
+        if (points.length == 0) {
+            return result;
+        }
+
+        Point closestPoint = null;
+        var closestDistance = Integer.MAX_VALUE;
+        while (true) {
+            for (var point : points) {
+                if (point == currentPos) continue;
+                if (result.contains(point)) continue;
+
+                int distance = (int)(Math.pow(point.getX() - currentPos.getX(), 2) + Math.pow(point.getY() - currentPos.getY(), 2));
+                if (distance < closestDistance) {
+                    closestPoint = point;
+                    closestDistance = distance;
+                }
+            }
+
+            if (closestPoint == null) {
+                result.add(startPos);
+                break;
+            } else {
+                result.add(closestPoint);
+                closestPoint = null;
+                closestDistance = Integer.MAX_VALUE;
+            }
+        }
+        return result;
     }
 }
--- Lab11/src/academy/pocu/comp3500/lab11/app/Program.java
+++ Lab11/src/academy/pocu/comp3500/lab11/app/Program.java
@@ -1,8 +1,70 @@
 package academy.pocu.comp3500.lab11.app;
 
+import academy.pocu.comp3500.lab11.BallBoy;
+import academy.pocu.comp3500.lab11.data.Point;
+
+import java.util.List;
+
 public class Program {
 
     public static void main(String[] args) {
-        // write your code here
+        String startPoint = new Point(0, 0).toString();
+
+        {
+            Point[] points = {};
+
+            List<Point> path = BallBoy.findPath(points);
+
+            assert (path.size() == 1);
+
+            String path0 = path.get(0).toString();
+
+            assert (path0.equals(startPoint));
+        }
+
+        {
+            Point[] points = {
+                    new Point(-3, -4)
+            };
+            String ballA = points[0].toString();
+
+            List<Point> path = BallBoy.findPath(points);
+
+            assert (path.size() == points.length + 2);
+
+            String path0 = path.get(0).toString();
+            String path1 = path.get(1).toString();
+            String path2 = path.get(2).toString();
+
+            assert (path0.equals(startPoint));
+            assert (path1.equals(ballA));
+            assert (path2.equals(startPoint));
+        }
+
+        {
+            Point[] points = {
+                    new Point(20, 15),
+                    new Point(20, 48),
+                    new Point(0, 63)
+            };
+            String ballA = points[0].toString();
+            String ballB = points[1].toString();
+            String ballC = points[2].toString();
+
+            List<Point> path = BallBoy.findPath(points);
+
+            assert (path.size() == points.length + 2);
+
+            String path0 = path.get(0).toString();
+            String path1 = path.get(1).toString();
+            String path2 = path.get(2).toString();
+            String path3 = path.get(3).toString();
+            String path4 = path.get(4).toString();
+
+            assert (path0.equals(startPoint));
+            assert ((path1.equals(ballA) && path2.equals(ballB) && path3.equals(ballC))
+                    || (path1.equals(ballC) && path2.equals(ballB) && path3.equals(ballA)));
+            assert (path4.equals(startPoint));
+        }
     }
 }
