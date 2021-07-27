f007eda600dc0ca68f7d863f5073e6e8b655426b
John <twglhk@gmail.com>
2021-07-27 22:23:02 +0900

    update : lab11 init

---
+++ Lab11/src/academy/pocu/comp3500/lab11/BallBoy.java
@@ -0,0 +1,23 @@
+package academy.pocu.comp3500.lab11;
+
+import academy.pocu.comp3500.lab11.data.Point;
+
+import java.util.List;
+//LinkedList
+//ArrayList
+//Stack
+//PriorityQueue
+//Hashmap
+//java.util.Arrays, java.util.Collections에서는 sort() 메서드만 사용할 수 있습니다.
+
+public class BallBoy {
+    public static List<Point> findPath(final Point[] points) {
+        //이 메서드는 다음의 인자를 받습니다.
+        //주워야 하는 골프공의 위치들: Point[] points
+        //적당히 짧은 최단 경로를 반환합니다.
+        //볼보이는 언제나 x = 0, y = 0 위치에서 시작하며 일이 끝나면 이 위치로 다시 돌아와야 합니다. 즉, 반환된 경로의 시작과 끝은 언제나 이 위치여야 합니다.
+        //시작 위치에 골프공이 있는 경우는 없습니다.
+
+        return null;
+    }
+}
