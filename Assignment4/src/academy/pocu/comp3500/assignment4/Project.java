d4fa261129d30046982ff02b88d33532dac5d037
John <twglhk@gmail.com>
2021-07-30 21:37:17 +0900

    update : ass4 init

---
+++ Assignment4/src/academy/pocu/comp3500/assignment4/Project.java
@@ -0,0 +1,28 @@
+package academy.pocu.comp3500.assignment4;
+
+import academy.pocu.comp3500.assignment4.project.Task;
+
+public final class Project {
+    //사용 가능 자료 구조
+    //LinkedList
+    //ArrayList
+    //Stack
+    //PriorityQueue
+    //Hashmap
+
+    public Project(final Task[] tasks) {
+
+    }
+
+    public int findTotalManMonths(final String task) {
+        return -1;
+    }
+
+    public int findMinDuration(final String task) {
+        return -1;
+    }
+
+    public int findMaxBonusCount(final String task) {
+        return -1;
+    }
+}
